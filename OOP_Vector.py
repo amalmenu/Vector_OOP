e478af0d47358751773e9cb1901c2e7a7ad8d456
amalelwatta <amalmenuka@gmail.com>
2024-03-19 14:27:31 +0100

    check 3

--- OOP_Vector.py
+++ OOP_Vector.py
@@ -1,7 +1,7 @@
 class Vector:
     def init(self, d):
 #Create d - dimensional vector of zeros
-        self.coords = [0]*d
+        self.coords =[0]*d
     def len (self):
 #Return the dimension of the vector
      return len(self.coords)
